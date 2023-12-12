609d833bc8439ea0a8eacf1116fde86b27eb4eb5
Lucas Chabiron <84028247+lucaschabiron@users.noreply.github.com>
2023-12-12 17:01:56 +0100

    Initial Commit

---
+++ Note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Note
+//
+//  Created by Lucas Chabiron on 12/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
