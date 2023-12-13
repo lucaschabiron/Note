1e3d4c19b47bdccfa0eedffff3f62b7fa4fad448
Lucas Chabiron <84028247+lucaschabiron@users.noreply.github.com>
2023-12-13 09:52:41 +0100

    feat: recent releases collection

---
+++ Note/AlbumDetails.swift
@@ -0,0 +1,16 @@
+//
+//  AlbumDetails.swift
+//  Note
+//
+//  Created by Lucas Chabiron on 13/12/2023.
+//
+
+import UIKit
+
+class AlbumDetails: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
--- Note/ViewController.swift
+++ Note/ViewController.swift
@@ -18,7 +18,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var featuredAlbums: UICollectionView!
     
     
-        var albums: [Album] = [Album(imageURL: "https://i.scdn.co/image/ab67616d00001e02045fc920ecf4f8094888ec26", name: "Illmatic", artistName: "Nas"),Album(imageURL: "https://i.scdn.co/image/ab67616d00001e02045fc920ecf4f8094888ec26", name: "Illmatic", artistName: "Nas"),Album(imageURL: "https://i.scdn.co/image/ab67616d00001e02045fc920ecf4f8094888ec26", name: "Illmatic", artistName: "Nas"),Album(imageURL: "https://i.scdn.co/image/ab67616d00001e02045fc920ecf4f8094888ec26", name: "Illmatic", artistName: "Nas")]
+    @IBOutlet weak var recentReleases: UICollectionView!
+    
+    var featured: [Album] = [Album(imageURL: "https://i.scdn.co/image/ab67616d00001e02045fc920ecf4f8094888ec26", name: "Illmatic", artistName: "Nas"),Album(imageURL: "https://i.scdn.co/image/ab67616d00001e02045fc920ecf4f8094888ec26", name: "Illmatic", artistName: "Nas"),Album(imageURL: "https://i.scdn.co/image/ab67616d00001e02045fc920ecf4f8094888ec26", name: "Illmatic", artistName: "Nas"),Album(imageURL: "https://i.scdn.co/image/ab67616d00001e02045fc920ecf4f8094888ec26", name: "Illmatic", artistName: "Nas")]
+    
+    var recents: [Album] = [Album(imageURL: "https://i.scdn.co/image/ab67616d00001e027db8828819494cf6d0b79fc4", name: "Illmatic", artistName: "Nas"),Album(imageURL: "https://i.scdn.co/image/ab67616d00001e027db8828819494cf6d0b79fc4", name: "Illmatic", artistName: "Nas"),Album(imageURL: "https://i.scdn.co/image/ab67616d00001e027db8828819494cf6d0b79fc4", name: "Illmatic", artistName: "Nas"),Album(imageURL: "https://i.scdn.co/image/ab67616d00001e027db8828819494cf6d0b79fc4", name: "Illmatic", artistName: "Nas")]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -29,16 +33,30 @@ class ViewController: UIViewController {
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        albums.count
+        if collectionView == featuredAlbums {
+            return featured.count
+        } else {
+            return recents.count
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let album = albums[indexPath.row]
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "featuredAlbum", for: indexPath) as! CollectionViewCell
-        cell.image.load(url: album.imageURL)
-        cell.layer.cornerRadius = 2
-        cell.layer.masksToBounds = true
-        return cell
+        if collectionView == featuredAlbums{
+            let album = featured[indexPath.row]
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "featuredAlbum", for: indexPath) as! CollectionViewCell
+            cell.image.load(url: album.imageURL)
+            cell.layer.cornerRadius = 4
+            cell.layer.masksToBounds = true
+            return cell}
+        if collectionView == recentReleases{
+            let album = recents[indexPath.row]
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "recentAlbum", for: indexPath) as! CollectionViewCell
+            cell.image.load(url: album.imageURL)
+            cell.layer.cornerRadius = 4
+            cell.layer.masksToBounds = true
+            return cell
+        }
+        return UICollectionViewCell()
     }
     
     
